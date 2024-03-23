cc3be9cc02a622b6a5b83cadf3fe7cc221eb9eda
Shubham Sharma <shbh541@gmail.com>
2024-03-24 00:44:08 +0530

    fixed line issues

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const ballDimension = { height: 4, width: 1 };
+export const ballDimension = { height: 4, width: 1.5 };
 export const squareDimension={ height: 15, width: 5 };
 export const increment = 5;
